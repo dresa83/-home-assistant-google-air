d7e6ac4a822201c9add222d8f3e708ebba7163b2
Andreas Hess <46423483+dresa83@users.noreply.github.com>
2025-03-12 11:43:17 +0000

    With new sensors

--- custom_components/google_air_quality/sensor.py
+++ custom_components/google_air_quality/sensor.py
@@ -14,46 +14,44 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     api_key = entry.data.get("api_key")
     latitude = entry.data.get("latitude")
     longitude = entry.data.get("longitude")
-    async_add_entities([GoogleAirQualitySensor(api_key, latitude, longitude)])
+
+    sensors = [
+        GoogleAirQualitySensor(api_key, latitude, longitude, "aqi", "AQI"),
+        GoogleAirQualitySensor(api_key, latitude, longitude, "pm2_5", "PM2.5"),
+        GoogleAirQualitySensor(api_key, latitude, longitude, "pm10", "PM10"),
+        GoogleAirQualitySensor(api_key, latitude, longitude, "co", "CO"),
+        GoogleAirQualitySensor(api_key, latitude, longitude, "ozone", "Ozone"),
+        GoogleAirQualitySensor(api_key, latitude, longitude, "no2", "NO2"),
+        GoogleAirQualitySensor(api_key, latitude, longitude, "so2", "SO2")
+    ]
+    async_add_entities(sensors)
 
 class GoogleAirQualitySensor(Entity):
     """Representation of a Google Air Quality sensor."""
 
-    def __init__(self, api_key, latitude, longitude):
+    def __init__(self, api_key, latitude, longitude, sensor_type, name):
         """Initialize the sensor."""
         self._api_key = api_key
         self._latitude = latitude
         self._longitude = longitude
+        self._sensor_type = sensor_type
+        self._name = name
         self._state = None
         self._attributes = {}
-        self._attr_name = "Google Air Quality"
+
+    @property
+    def name(self):
+        return f"Google Air Quality {self._name}"
 
     @property
     def state(self):
-        """Return the state of the sensor (numeric AQI)."""
         return self._state
 
     @property
     def extra_state_attributes(self):
-        """Return additional attributes."""
         return self._attributes
 
-    @property
-    def icon(self):
-        """Set custom icon based on AQI value."""
-        if self._state is not None:
-            if self._state <= 50:
-                return "mdi:emoticon-happy"
-            elif self._state <= 100:
-                return "mdi:emoticon-neutral"
-            elif self._state <= 150:
-                return "mdi:emoticon-sad"
-            else:
-                return "mdi:emoticon-dead"
-        return "mdi:cloud-question"
-
     async def async_update(self):
-        """Fetch new data from the API."""
         url = f"{API_URL}?key={self._api_key}"
         payload = {
             "universalAqi": True,
@@ -82,26 +80,25 @@ class GoogleAirQualitySensor(Entity):
                         return
 
                     data = await response.json()
-                    _LOGGER.debug(f"API Response: {data}")
-
-                    indexes = data.get("indexes", [{}])[0]
                     pollutants = {pollutant["code"]: pollutant for pollutant in data.get("pollutants", [])}
 
-                    self._state = indexes.get("aqi", "Unknown")
-                    self._attributes = {
-                        "aqi_display": indexes.get("aqiDisplay", "Unknown"),
-                        "category": indexes.get("category", "Unknown"),
-                        "dominant_pollutant": indexes.get("dominantPollutant", "Unknown"),
-                        "region_code": data.get("regionCode", "Unknown"),
-                        "date_time": data.get("dateTime", "Unknown"),
-                        "pm2_5": pollutants.get("pm25", {}).get("concentration", {}).get("value", "Unknown"),
-                        "pm10": pollutants.get("pm10", {}).get("concentration", {}).get("value", "Unknown"),
-                        "co": pollutants.get("co", {}).get("concentration", {}).get("value", "Unknown"),
-                        "ozone": pollutants.get("o3", {}).get("concentration", {}).get("value", "Unknown"),
-                        "no2": pollutants.get("no2", {}).get("concentration", {}).get("value", "Unknown"),
-                        "so2": pollutants.get("so2", {}).get("concentration", {}).get("value", "Unknown"),
-                        "health_recommendations": data.get("healthRecommendations", {})
-                    }
+                    if self._sensor_type == "aqi":
+                        indexes = data.get("indexes", [{}])[0]
+                        self._state = indexes.get("aqi", "Unknown")
+                        self._attributes = {
+                            "category": indexes.get("category", "Unknown"),
+                            "dominant_pollutant": indexes.get("dominantPollutant", "Unknown"),
+                            "region_code": data.get("regionCode", "Unknown"),
+                            "date_time": data.get("dateTime", "Unknown"),
+                            "health_recommendations": data.get("healthRecommendations", {})
+                        }
+                    else:
+                        self._state = pollutants.get(self._sensor_type, {}).get("concentration", {}).get("value", "Unknown")
+                        self._attributes = {
+                            "unit": pollutants.get(self._sensor_type, {}).get("concentration", {}).get("units", "Unknown"),
+                            "sources": pollutants.get(self._sensor_type, {}).get("additionalInfo", {}).get("sources", "Unknown"),
+                            "effects": pollutants.get(self._sensor_type, {}).get("additionalInfo", {}).get("effects", "Unknown")
+                        }
             except aiohttp.ClientError as e:
                 self._state = "Error"
                 self._attributes = {"error": str(e)}
